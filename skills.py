c1a24a89cbfc60c9bf3da257c8c2ae8a61114709
Tom Cooper <tacooper@vt.edu>
2020-05-19 20:19:24 -0400

    Separated embedded C/C++ skills.

--- skills.py
+++ skills.py
@@ -8,9 +8,10 @@ class TableEntry():
         self.proficiency = proficiency
 
 class Skill(enum.Enum):
-    C_CPP = TableEntry("C/C++", 90)
+    EMBEDDED_C = TableEntry("Embedded C", 90)
     PYTHON = TableEntry("Python", 90)
     JAVA = TableEntry("Java", 90)
+    CPP = TableEntry("C++", 70)
     BASH_SHELL = TableEntry("Bash Shell", 70)
     GIT = TableEntry("Git", 70)
     ANDROID = TableEntry("Android", 50)
