ad6aa3a3d32c83109bdabcafb562670eabb211ae
Tom Cooper <tacooper@vt.edu>
2020-05-20 22:11:32 -0400

    Stylized skills/tools when generating projects.

--- generate_projects.py
+++ generate_projects.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import os
+from data.skills import Skill
+from data.tools import Tool
 
 # define constants
 PROJECTS_DIR = "data/projects"
@@ -13,8 +15,18 @@ def main():
 
         # open each project file in read-only mode
         with open(path_name, "r") as file:
-            # display text from each project file in bulleted list
+            # read text from each project file
             text = file.read()
+
+            # replace all skill enum names with stylized values in each project file
+            for skill in Skill:
+                text = text.replace("[{}]".format(skill.name), "`{}`".format(skill.value.name))
+
+            # replace all tool enum names with stylized values in each project file
+            for tool in Tool:
+                text = text.replace("[{}]".format(tool.name), "`{}`".format(tool.value))
+
+            # display stylized text from each project file in bulleted list
             print("* {}".format(text))
 
 if __name__ == "__main__":
